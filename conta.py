bbc1f8bc19b03acc865ab59b2ee5b9442df51600
WeslleiBrito <wesllei.wbs@gmail.com>
2021-11-20 15:59:39 -0300

    Adicionado os metodos, deposita, sacar e extrato

--- conta.py
+++ conta.py
@@ -6,3 +6,12 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f'A conta do titular {self.titular}, possui um saldo de {self.saldo} reais')
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
