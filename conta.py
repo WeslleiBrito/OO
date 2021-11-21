d22fee3886627fa8ab44aa69bfbc8c1b30a23cb7
WeslleiBrito <wesllei.wbs@gmail.com>
2021-11-20 21:06:45 -0300

    definindo atributos privados

--- conta.py
+++ conta.py
@@ -2,16 +2,16 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'A conta do titular {self.titular}, possui um saldo de {self.saldo} reais')
+        print(f'A conta do titular {self.__titular}, possui um saldo de {self.__saldo} reais')
 
     def deposito(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
